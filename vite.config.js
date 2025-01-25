5c486d30902015cc934fa02ec3191c958cf9723e
Krish Bharadwaj <krishbharadwaj43@gmail.com>
2025-01-25 15:59:50 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
+// vite.config.js
 export default defineConfig({
-  plugins: [react()],
-})
+  server: {
+    host: "0.0.0.0",  // This ensures Vite binds to all network interfaces
+    port: process.env.PORT || 3000, // Render assigns a port dynamically
+  },
+});
